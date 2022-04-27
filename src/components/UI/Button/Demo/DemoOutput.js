949bab85ef76dd6d6b1822079d49c771a23d004b
jakesungjaekim <codejakecarry@gmail.com>
2022-04-27 10:00:38 +0900

    first msg

---
+++ src/components/UI/Button/Demo/DemoOutput.js
@@ -0,0 +1,9 @@
+import React from "react";
+import MyParagraph from "./MyParagraph";
+
+const DemoOutput = (props) => {
+  console.log("DEMO OUTPUT");
+  return <MyParagraph>{props.show ? "this is new" : ""}</MyParagraph>;
+};
+
+export default React.memo(DemoOutput);
